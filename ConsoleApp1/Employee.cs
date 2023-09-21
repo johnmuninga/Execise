c1a99dbd74356100ec50058c4f4ffd89b381a7f1
johnmuninga <johnmuninga03@gmail.com>
2023-09-21 14:36:42 +0200

    exercise

---
+++ ConsoleApp1/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Employee
+    {
+        //Attributes field
+        string name;
+        string surname;
+        string departName;
+        double sales;
+
+        //Construtor
+
+        public Employee()
+        {
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public string getSurName()
+        {
+            return surname;
+        }
+        public string getDepartName()
+        {
+            return departName;
+        }
+
+
+        public void setName(string name)
+        {
+            this.name = Char.ToUpper(name[0]).ToString() + name.Substring(1).ToLower();
+        }
+        public void setSurname(string surname)
+        {
+            this.surname = surname.ToUpper();
+        }
+
+        public void setDepartName(char dept)
+        {
+            if (dept == 'E')
+            {
+                this.departName = "Electronics";
+            }
+            else if (dept == 'H')
+            {
+                this.departName = "Hardware";
+            }
+            else if (dept == 'S')
+            {
+                this.departName = "Sportswear";
+            }
+            else
+            {
+                Console.WriteLine("Wrong Parameter Given !");
+            }
+        }
+        public void setSales(double sales)
+        {
+            if (sales < 2000)
+            {
+                this.sales = 2000;
+
+            }
+            else
+            {
+                this.sales = sales;
+            }
+        }
+        public double calcSalary()
+        {
+            double salary = 3000;
+
+            if (departName == "Hardware")
+            {
+                salary += sales * 0.02;
+            }
+            else if (departName == "Electronics")
+            {
+                salary += sales * 0.035;
+            }
+            else
+            {
+                salary += sales * 0.045;
+            }
+
+            return salary;
+        }
+    }
+
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Enter Name: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Enter surName: ");
+            //string surName = Console.ReadLine();
+            //Console.Write("Enter DepartName: ");
+            //string DepartName = Console.ReadLine();
+            //Console.Write("Enter Sales: ");
+            //double sales = double.Parse(Console.ReadLine());
+            ////Object
+            //Employee obj = new Employee(name, surName, DepartName, sales);
+
+            //obj.getName();
+            //obj.getSurName();
+            //Console.WriteLine(obj.setName());
+            //Console.WriteLine(obj.setSurname());
+
+
+            Employee[] employees = new Employee[2];
+            
+
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                Console.Write($"Enter the name of employee {i+1}:");
+                string emp_name = Console.ReadLine();
+                employees[i] = new Employee();
+                employees[i].setName(emp_name);
+
+                Console.Write($"Enter the surname of employee {i + 1}:");
+                string emp_surname = Console.ReadLine();
+                employees[i].setSurname(emp_surname);
+
+                Console.Write($"Enter the department char of employee {i + 1}:");
+                char emp_dept_name = char.Parse(Console.ReadLine());
+                employees[i].setDepartName(emp_dept_name);
+
+                Console.Write($"Enter the sales of employee {i + 1}:");
+                double emp_sales = double.Parse(Console.ReadLine());
+                employees[i].setSales(emp_sales);
+
+            }
+            Console.WriteLine();
+            foreach (Employee employee in employees)
+            {
+                Console.WriteLine($"Name:{employee.getName()}");
+                Console.WriteLine($"Surname:{employee.getSurName()}");
+                Console.WriteLine($"Department:{employee.getDepartName()}");
+                Console.WriteLine($"Salary:{employee.calcSalary()}");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
